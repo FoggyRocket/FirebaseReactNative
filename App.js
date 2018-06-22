b84b7dba5fe07d24c400ee5a31e0b99d4535afd0
Foggyrocket <dyl.blink.1.8.2@gmail.com>
2018-06-22 16:58:30 -0500

    Ya estufas todo completo

--- App.js
+++ App.js
@@ -9,39 +9,53 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View,
+  KeyboardAvoidingView,
   Alert
 } from 'react-native';
-import {Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body,Item,Icon,Input,}  from 'native-base'
+import {Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body,Item,Icon,Input,Spinner}  from 'native-base'
 import {ListComponent} from "./src/component/ListComponent";
+import * as firebase from 'firebase';
 
 
 
 type Props = {};
 export default class App extends Component<Props> {
   state={
-    lista:[
-        {name:'Perro'},
-        {name:"Gato"},
-        {name:"Delfin"},
-        {name:"Pikachu"}
-    ],
+    lista:[],
     item:'',
-      ID:null,
+      elId:null,
   }
+
+    componentWillMount(){
+      firebase.database().ref("listita")
+          .once("value")
+          .then(snap=>{
+              this.setState({lista:snap.val()})
+          })
+  }
+
+  //
   handleChange=(item)=>{
     console.log("aqui va el item", item)
       this.setState({item:item})
   }
+  //
   Send=()=>{
       let {item,lista}=this.state;
       var ob = {name:item}
       lista.push(ob)
       this.setState({lista:lista, item:''})
-      console.log("Enviare",ob)
+      //Hack para no pelear con los ids
+      this.firebaseUpdate()
   }
+  firebaseUpdate=()=>{
+      let {lista}=this.state
+      var updates={listita:lista}
+      firebase.database().ref().update(updates)
+  }
+  //
   confirmDelete=(i)=>{
-      this.setState({ID:i})
+      this.setState({elId:i})
       const message = '¿Seguro que quieres eliminarlo?';
       Alert.alert(
           'ELIMINAR',
@@ -58,17 +72,20 @@ export default class App extends Component<Props> {
           ]
       )
   }
+  //
   deleteItem=()=>{
-        let{lista,ID}=this.state
-        lista.splice(ID,1)
+        let{lista,elId}=this.state
+        lista.splice(elId,1)
        this.setState({lista:lista})
+      this.firebaseUpdate()
 
   }
 
   render() {
-
+    let {lista,item}=this.state
     return (
-        <Container>
+
+        <Container >
             <Header>
               <Left>
                 <Button transparent>
@@ -77,7 +94,7 @@ export default class App extends Component<Props> {
               </Left>
               <Body>
                 <Title>
-                  Firebase + RN
+                  Firebase
                 </Title>
               </Body>
               <Right/>
@@ -89,23 +106,28 @@ export default class App extends Component<Props> {
                     value={this.state.item}
                     onChangeText={this.handleChange}
                   />
-                  <Button transparent onPress={this.Send}>
+                  <Button transparent onPress={this.Send} disabled={item === '' ? true : false }>
                       <Icon active name='send' />
                   </Button>
               </Item>
-            <ListComponent
-                lista={this.state.lista}
-                onDelete={this.confirmDelete}
-            />
+              {lista.length < 1 ?
+                  <Spinner color="blue"/>
+                  :
+                  <ListComponent
+                        lista={lista}
+                        onDelete={this.confirmDelete}
+                    />
+              }
+
           </Content>
 
-          <Footer>
-            <FooterTab>
-              <Button full>
-                <Text>Footer</Text>
-              </Button>
-            </FooterTab>
-          </Footer>
+            <Footer>
+                <FooterTab>
+                    <Button full>
+                        <Text>React Native</Text>
+                    </Button>
+                </FooterTab>
+            </Footer>
         </Container>
     );
   }
@@ -125,5 +147,16 @@ const styles = StyleSheet.create({
   },
   elinput:{
       marginBottom:20
-  }
+  },
+
 });
+//Instalación de Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDzgZapMUPFkg6dlte4W3au-E0zHR7Vp50",
+    authDomain: "test-4341e.firebaseapp.com",
+    databaseURL: "https://test-4341e.firebaseio.com",
+    projectId: "test-4341e",
+    storageBucket: "test-4341e.appspot.com",
+    messagingSenderId: "301332539094"
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
